78ed8cf9da07dabb0b4a53533b0e6339e546f7c6
alexanderzabiela <alexanderzabiela@gmail.com>
2024-10-16 08:53:45 +0100

    CORS in Package.JSON

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 import { authorisationMiddleware } from './middleware/authorisationMiddleware';
 import timeRouter from './routes/time';
 import metricsRouter from './routes/metrics';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Enable CORS
+app.use(cors());
 
 // Apply routes with authorisation middleware
 app.use('/', authorisationMiddleware, timeRouter);
