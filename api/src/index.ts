95afd2335066c4a104db187ebd3f8aa72b0c021a
alexanderzabiela <alexanderzabiela@gmail.com>
2024-10-16 08:51:02 +0100

    Added CORS and updated routes

--- api/src/index.ts
+++ api/src/index.ts
@@ -7,8 +7,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Apply routes with authorisation middleware
-app.use('/time', authorisationMiddleware, timeRouter);
-app.use('/metrics', authorisationMiddleware, metricsRouter);
+app.use('/', authorisationMiddleware, timeRouter);
+app.use('/', authorisationMiddleware, metricsRouter);
 
 // Start server
 app.listen(PORT, () => {
